6387666f2966a6280d299732c79fb37083919605
Ryan Taylor <ryant030408@gmail.com>
2017-11-08 22:35:02 -0500

    updates

--- sentdex_tutorial/regression_tp2.py
+++ sentdex_tutorial/regression_tp2.py
@@ -4,7 +4,12 @@ import quandl
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
-import warnings
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+style.use('ggplot')
 
 
 df = quandl.get('WIKI/GOOGL')
@@ -25,20 +30,57 @@ df.fillna(-99999, inplace=True)
 
 # gets to ceiling of days out, predict 10% of data frame
 forecast_out = int(math.ceil(0.01 * len(df)))
-
+# print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
+
 # print(df.head())
 #features are everything except label
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
+
 #scal x before feeding through classifier
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])
 
+#shuffles data
 X_train, X_test,y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
-#video 4
+clf = LinearRegression(n_jobs=-1)
+# clf = svm.SVR()
+# clf = svm.SVR(kernel='poly')
 
+
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+accuracy = clf.score(X_test, y_test)
+# print(accuracy)
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df["Forecast"] = np.nan
+#y is on the graph but the date is not
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+print(df.tail())
+df["Adj. Close"].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
---
+++ siraj_tutorial/citynamenn.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, division, print_function
+import os
+# LSTM(long short term memory network) to generate city names
+import ssl
+import tflearn
+from six import moves
+from tflearn.data_utils import *
+
+
+# step 1 - retrieve data
+path = "US_cities.txt"
+
+if not os.path.isfile(path):
+    context = ssl._create_unverified_context()
+    # get data set
+    moves.urllib.request.urlretrieve(
+        "https://raw.githubusercontent.com/tflearn/tflearn.github.io/master/resources/US_Cities.txt", path,
+        context=context)
+
+# city name max length
+maxlen = 20
+
+# vector-ize text file, redundant step means we will go through 3 sequences to make it happen
+X, Y, char_idx = textfile_to_semi_redundant_sequences(path, seq_maxlen=maxlen, redun_step=3)
+
+# create LSTM
+# saying make our inout layer, 20 cahracters
+g = tflearn.input_data(shape=[None, maxlen, len(char_idx)])
+# now create lstm later, 512 nodes(neurons)
+g = tflearn.lstm(g, 512, return_seq=True)
+# create dropout, when we train nn data is flowing, this drops over fitting(cant generalize data to new info) so
+# nodes are randomly turned off, break out of 'groves'
+g = tflearn.dropout(g, 0.5)
+# create another lstm layer
+g = tflearn.lstm(g, 512)
+# add more dropout for every layer, 0.5 is just a measure of how random we want to get
+g = tflearn.dropout(g, 0.5)
+# last layer, softmax is a type of logistic regression
+g = tflearn.fully_connected(g, len(char_idx), activation='softmax')
+
+
+# generate cities
+# checkpoints save model status
+m = tflearn.SequenceGenerator(g, dictionary=char_idx, seq_maxlen=maxlen, clip_gradients=5.0,
+                              checkpoint_path='model_us_cities')
+
+#training
+for i in range(40):
+    # seed helps us start from the same time every time we generate
+    seed = random_sequence_from_textfile(path, maxlen)
+    # this is the training
+    m.fit(X, Y, validation_set=0.1, batch_size=128, n_epoch=1, run_id='us_cities')
+    print("TESTING")
+    print(m.generate(30, temperature=1.2, seq_seed=seed))
+    print("TESTING")
+    print(m.generate(30, temperature=1.0, seq_seed=seed))
+    print("TESTING")
+    print(m.generate(30, temperature=0.5, seq_seed=seed))
